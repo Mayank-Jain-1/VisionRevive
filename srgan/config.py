3356ca30d4fe2feb348696c2178bab9a9a13ec60
Mayank <mayankjainx404@gmail.com>
2023-11-23 02:03:53 +0530

    Created a Config file for the project

---
+++ srgan/config.py
@@ -0,0 +1,17 @@
+import torch
+
+
+# Device Parameters
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Data Loader parameters
+BATCH_SIZE = 250
+NUM_WORKERS = 0
+
+# Generator hyper parameters
+NUM_CHANNELS = 64
+NUM_BLOCKS = 16
+GEN_LR = 0.01
+
+# Discriminator parameteres
+DISC_LR = 0.01
